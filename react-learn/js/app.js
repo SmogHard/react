5a50275fde0f705b4bee2bbefe37395ed94c608e
Ruslan Isaev <isaev@lanbilling.ru>
2017-06-01 17:47:35 +0300

    Закончи день

--- react-learn/js/app.js
+++ react-learn/js/app.js
@@ -1,26 +1,54 @@
 var my_news = [
   {
     author: 'Саша Печкин',
-    text: 'В четверг, четвертого числа...'
+    text: 'В четчерг, четвертого числа...',
+    bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
   },
   {
     author: 'Просто Вася',
-    text: 'Считаю, что $ должен стоить 35 рублей!'
+    text: 'Считаю, что $ должен стоить 35 рублей!',
+    bigText: 'А евро 42!'
+  },
+  {
+    author: 'Гость',
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+    bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
   }
 ];
 
 var Article = React.createClass({
-  render: function() {
-    var author = this.props.data.author,
-        text = this.props.data.text;
+	propTypes: {
+        data: React.PropTypes.shape({
+          author: React.PropTypes.string.isRequired,
+          text: React.PropTypes.string.isRequired,
+          bigText: React.PropTypes.string.isRequired
+        })
+    },
+    getInitialState: function() {
+        return {
+          visible: false
+        };
+    },
+    render: function() {
+        var author = this.props.data.author,
+            text = this.props.data.text,
+            bigText = this.props.data.bigText;
+            visible = this.state.visible;
+    
+        return (
+          <div className="article">
+            <p className="news__author">{author}:</p>
+            <p className="news__text">{text}</p>
 
-    return (
-      <div className="article">
-        <p className="news__author">{author}:</p>
-        <p className="news__text">{text}</p>
-      </div>
-    )
-  }
+            {/* для ссылки readmore: не показывай ссылку, если visible === true */}
+            <a href="#" className={'news__readmore ' + (visible ? 'none': '')}>Подробнее</a>
+
+            {/* для большо текста: не показывай текст, если visible === false */}
+            <p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
+
+          </div>
+        )
+    }
 });
 
 var News = React.createClass({
